a8f26fc64de19ab647518d156686b8a29e031264
sandeepvaranasi1 <sandeepvaranasi568@gmail.com>
2025-04-02 22:13:24 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import Home from "./pages/Home";
+import RecipeDetails from "./pages/ReceipeDetails";
+import AddEditRecipe from "./pages/AddEditRecipe";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CardHeader, Container } from "@mui/material";
+import { ToastContainer } from "react-toastify";
 
-function App() {
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#00A36C",
+    },
+  },
+  typography: {
+    fontFamily: "Kanit",
+  },  
+});
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <Provider store={store}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/recipe/:id" element={<RecipeDetails />} />
+              <Route path="/add" element={<AddEditRecipe />} />
+              <Route path="/edit/:id" element={<AddEditRecipe />} />
+            </Routes>
+          </Router>
+          <ToastContainer />
+        </ThemeProvider>
+      </Provider>
+    </Container>
   );
-}
+};
 
 export default App;
